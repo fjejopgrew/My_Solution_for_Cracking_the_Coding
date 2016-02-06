5679dc9f97ff672b0e4a50b69d1a7cea3d9ae0cf
Hang Gao <hgao19@hawk.iit.edu>
2016-02-05 22:19:45 -0600

    Update 1.2.java

--- Chapter1/1.2.java
+++ Chapter1/1.2.java
@@ -13,30 +13,6 @@ public class isPermutation {
 		return sort(a).equals(sort(b));
 	}
 	
-	public static boolean isPermutation2(String a, String b)
-	{
-		if (a.length()!=b.length())
-			return false;
-		
-		int[] letters=new int[128];
-		
-		char[] s_array = a.toCharArray();
-		for(char c:s_array)
-		{
-			letters[c]++;
-		}
-		
-		for(int i=0;i<b.length();i++)
-		{
-			int c=(int) b.charAt(i);
-			letters[c]--;
-			if(letters[c]<0)
-			return false;
-		}
-		
-		return true;
-	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[][] pairs={{"apple", "papel"}, {"carrot", "tarroc"}, {"aahh", "hhhh"}};
