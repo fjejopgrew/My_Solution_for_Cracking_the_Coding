2300aa4ae2b6178ac58f37f5b45beeb2db2583e7
Hang Gao <hgao19@hawk.iit.edu>
2016-02-05 21:28:17 -0600

    Create 2.8.java

---
+++ Chapter2/2.8.java
@@ -0,0 +1,50 @@
+
+public class Q8
+{
+	public static LinkedListNode findBegining(LinkedListNode head)
+	{
+		LinkedListNode fast=head;
+		LinkedListNode slow=head;
+		while(fast!=null&&fast.next!=null)
+		{
+			slow=slow.next;
+			fast=fast.next.next;
+			if(fast==slow)
+			break;
+		}		
+		
+		//error check-no meeting point, and therefore no loop
+		if(fast==null||fast.next==null)
+		{
+			return null;
+		}
+		
+		LinkedListNode start=head;
+		while(start!=slow)
+		{
+			slow=slow.next;
+			start=start.next;
+		}
+		
+		return slow;
+	}
+ 
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		LinkedListNode first = new LinkedListNode(0, null, null); // AssortedMethods.randomLinkedList(1000,
+		// 0, 2);
+		LinkedListNode head = first;
+		LinkedListNode second = first;
+		for (int i = 1; i < 10; i++)
+		{
+			second = new LinkedListNode(i, null, null);
+			first.setNext(second);
+			second.setPrevious(first);
+			first = second;
+		}
+		head.next.next.next.next=head.next.next;
+		System.out.println(findBegining(head).data);
+	}
+
+}
