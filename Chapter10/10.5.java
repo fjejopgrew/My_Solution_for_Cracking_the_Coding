77c5942587d44848ce6707ac9895bab55c017263
Hang Gao <hgao19@hawk.iit.edu>
2016-02-08 23:32:10 -0600

    Create 10.5.java

---
+++ Chapter10/10.5.java
@@ -0,0 +1,64 @@
+package Q5;
+
+/*********************************************/
+/***************** Problem 5 *****************/
+/*********************************************/
+public class sparseSearch
+{
+	public static int search(String[] str, String s, int first, int last)
+	{
+		if (first > last)
+			return -1;
+		// find mid
+		int mid = (first + last) / 2;
+
+		// if mid is empty, find closest non-empty string
+		if (str[mid].isEmpty())
+		{
+			int left = mid - 1;
+			int right = mid + 1;
+			while (true)
+			{
+				if (left < first && right > last)
+				{
+					return -1;
+				}
+				else if(right<=last&&!str[right].isEmpty())
+				{
+					mid=right;
+					break;
+				}
+				else if(left>=first&&!str[left].isEmpty())
+				{
+					mid=left;
+					break;
+				}
+				left--;
+				right++;
+			}
+		}
+		
+		//comepare tow strings
+		if(str[mid].equals(s))
+		{
+			return mid;
+		}
+		else if(str[mid].compareTo(s)<0)
+		{
+			return search(str, s, mid+1, last);
+		}
+		else 
+		{
+			return search(str, s, first, mid-1);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String[] str={"at","","","","ball","","","car","","","dad","","",};
+		String s="ball";
+		System.out.println(search(str, s,0,str.length));
+	}
+
+}
